cff3ebc636741e8424d1c078c9811db664675a8a
Mohd. Shafikur Rahman <shafikshaon@gmail.com>
2021-03-28 22:25:45 +0600

    basic vuetify project setup

---
+++ frontend/src/App.vue
@@ -0,0 +1,51 @@
+<template>
+  <div id="app">
+    <v-app>
+      <v-navigation-drawer app>
+        <!-- -->
+      </v-navigation-drawer>
+
+      <v-app-bar app>
+        <!-- -->
+      </v-app-bar>
+
+      <!-- Sizes your content based upon application components -->
+      <v-main>
+
+        <!-- Provides the application the proper gutter -->
+        <v-container fluid>
+
+          <!-- If using vue-router -->
+          <router-view></router-view>
+        </v-container>
+      </v-main>
+
+      <v-footer app>
+        <!-- -->
+      </v-footer>
+    </v-app>
+  </div>
+</template>
+
+<style lang="scss">
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+
+#nav {
+  padding: 30px;
+
+  a {
+    font-weight: bold;
+    color: #2c3e50;
+
+    &.router-link-exact-active {
+      color: #42b983;
+    }
+  }
+}
+</style>
